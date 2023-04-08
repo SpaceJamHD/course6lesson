0164f9f327ba4f3931373beb2f0c5474c4b5c672
*Ilya <fedaevi52@gmail.com>
2023-04-08 21:59:26 +0300

    modific

--- js.js
+++ js.js
@@ -10,3 +10,36 @@ const user = {
     },
     skills: ['html', 'css', 'js']
 }
+
+let value;
+let prop = 'email';
+
+
+value = user.firstName; // number 1
+value = user['isAdmin']; // number 2
+value = user['user-address']; // number 2
+value = user['user-address'].city; // number Влож
+value = user['user-address']['city'];
+value = user[prop][0];
+
+
+user.firstName = 'Den';
+
+value = user.firstName;
+
+user.info = 'Some info';
+
+value = user.info;
+
+value = user['user-address'].city = 'Kiev';
+value = user['user-address'].country = 'Ukraine';
+
+console.log(user.plan)
+user.plan = ();
+
+user.plan.basic = 'basic';
+
+
+
+console.log(value);
+console.log(user);
