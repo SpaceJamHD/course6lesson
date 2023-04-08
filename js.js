b62d786ac843b7328a36b68353a7138fc081fe47
*Ilya <fedaevi52@gmail.com>
2023-04-08 21:42:40 +0300

    create js and html

---
+++ js.js
@@ -0,0 +1,12 @@
+// Объекты
+
+const user = {
+    firstName: 'Denis',
+    age: 30,
+    isAdmin: true,
+    email: 'tes@test.com',
+    'user-address': {
+        city: 'Kharkiv'
+    },
+    skills: ['html', 'css', 'js']
+}
